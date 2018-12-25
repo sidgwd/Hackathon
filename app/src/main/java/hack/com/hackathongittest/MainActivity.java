f1e30ed82db05844d55640c1722175a59cee261f
nithin <nitinpoojary888@gmail.com>
2018-12-25 20:02:10 +0530

    Nithin Here

--- app/src/main/java/hack/com/hackathongittest/MainActivity.java
+++ app/src/main/java/hack/com/hackathongittest/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 //hello Alll howz it going
+    // Hey sid updated
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
